2171e7f03f29d64dc584b2bb811fa5b98784b061
VNM Evony <117020747+vnm1165@users.noreply.github.com>
2023-08-29 19:23:58 +0700

    Add files via upload

--- n2vi.js
+++ n2vi.js
@@ -1,101 +1,109 @@
 (function () {
-  const numberWords = {
-    units: ["?", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"],
-    tens: [
-      "lẻ",
-      "mười",
-      "hai mươi",
-      "ba mươi",
-      "bốn mươi",
-      "năm mươi",
-      "sáu mươi",
-      "bảy mươi",
-      "tám mươi",
-      "chín mươi",
-    ],
-    hundreds: [
-      "không trăm",
-      "một trăm",
-      "hai trăm",
-      "ba trăm",
-      "bốn trăm",
-      "năm trăm",
-      "sáu trăm",
-      "bảy trăm",
-      "tám trăm",
-      "chín trăm",
-    ],
+  var default_numbers = " hai ba bốn năm sáu bảy tám chín";
+  var dict = {
+    units: ("? một" + default_numbers).split(" "),
+    tens: ("lẻ mười" + default_numbers).split(" "),
+    hundreds: ("không một" + default_numbers).split(" "),
   };
+  const tram = "trăm";
+  var digits = "x nghìn triệu tỉ nghìn".split(" ");
 
-  const scales = ["", "nghìn", "triệu", "tỷ", "nghìn tỷ"];
-  const hundred = "trăm";
-
-  function getUnitsInWords(n) {
-    return numberWords.units[n];
-  }
-
-  function getTensInWords(n) {
-    return numberWords.tens[n];
-  }
-
-  function getHundredsInWords(n) {
-    return numberWords.hundreds[n];
-  }
-
-  function convertChunk(chunk, scaleIdx) {
-    if (chunk === "000") {
-      return "";
+  function tenth(block_of_2) {
+    var sl1 = dict.units[block_of_2[1]];
+    var result = [dict.tens[block_of_2[0]]];
+    if (block_of_2[0] > 0 && block_of_2[1] == 5) sl1 = "lăm";
+    if (block_of_2[0] > 1) {
+      result.push("mươi");
+      if (block_of_2[1] == 1) sl1 = "mốt";
     }
+    if (sl1 != "?") result.push(sl1);
+    return result.join(" ");
+  }
 
-    const [hundreds, tens, units] = chunk.split("").map(Number);
-    const words = [];
+  /**
+   * convert number in blocks of 3
+   * @param  {string} block "block of 3 mumbers"
+   * @return {string}   [description]
+   */
+  function block_of_three(block) {
+    switch (block.length) {
+      case 1:
+        return dict.units[block];
 
-    if (hundreds !== 0) {
-      words.push(getHundredsInWords(hundreds));
+      case 2:
+        return tenth(block);
+
+      case 3:
+        var result = [dict.hundreds[block[0]], tram];
+        if (block.slice(1, 3) != "00") {
+          var sl12 = tenth(block.slice(1, 3));
+          result.push(sl12);
+        }
+        return result.join(" ");
     }
+    return "";
+  }
+  /**
+   * Get number from unit, to string
+   * @param  {mixed} nStr input money
+   * @return {String}  money string, removed digits
+   */
+  function formatnumber(nStr) {
+    nStr += "";
+    var x = nStr.split(".");
+    var x1 = x[0];
+    var x2 = x.length > 1 ? "." + x[1] : "";
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+      x1 = x1.replace(rgx, "$1" + "," + "$2");
+    }
+    return x1 + x2;
+  }
 
-    if (tens !== 0 || units !== 0) {
-      if (tens === 0) {
-        words.push(getUnitsInWords(units));
-      } else if (tens === 1) {
-        words.push("mười", getUnitsInWords(units));
-      } else {
-        words.push(getTensInWords(tens), getUnitsInWords(units));
+  function digit_counting(i, digit_counter) {
+    var result = digits[i];
+
+    return result;
+  }
+  function to_vietnamese(input, currency) {
+    var str = parseInt(input) + "";
+    var index = str.length;
+    if (index == 0 || str == "NaN") return "";
+    var i = 0;
+    var arr = [];
+    var result = [];
+
+    //explode number string into blocks of 3numbers and push to queue
+    while (index >= 0) {
+      arr.push(str.substring(index, Math.max(index - 3, 0)));
+      index -= 3;
+    }
+    //loop though queue and convert each block
+    var digit_counter = 0;
+    var digit;
+    var adding;
+    for (i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] == "000") {
+        digit_counter += 1;
+        if (i == 2 && digit_counter == 2) {
+          result.push(digit_counting(i + 1, digit_counter));
+        }
+      } else if (arr[i] != "") {
+        digit_counter = 0;
+        result.push(block_of_three(arr[i]));
+        digit = digit_counting(i, digit_counter);
+        if (digit && digit != "x") result.push(digit);
       }
     }
-
-    if (scaleIdx > 0) {
-      words.push(scales[scaleIdx]);
-    }
-
-    return words.join(" ");
+    if (currency) result.push(currency);
+    //remove unwanted white space
+    return result.join(" ");
   }
 
-  function convertToVietnameseNumber(n) {
-    if (parseInt(n) === 0) {
-      return "không";
-    }
-    const numStr = parseInt(n).toString();
-    const chunks = [];
-    let chunk = "";
-
-    for (let i = numStr.length - 1, j = 0; i >= 0; i--, j++) {
-      chunk = numStr[i] + chunk;
-      if (j % 3 === 2 || i === 0) {
-        chunks.unshift(chunk);
-        chunk = "";
-      }
-    }
-
-    const words = chunks.map((chunk, idx) =>
-      convertChunk(chunk, chunks.length - 1 - idx)
-    );
-    return words.join(" ");
+  if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+    module.exports = to_vietnamese;
+  } else if (typeof window !== undefined) {
+    window.to_vietnamese = to_vietnamese;
   }
-
-  if (typeof module !== "undefined" && module.exports !== undefined) {
-    module.exports = convertToVietnameseNumber;
-  } else {
-    window.to_vietnamese = convertToVietnameseNumber;
-  }
+  return to_vietnamese;
 })();
